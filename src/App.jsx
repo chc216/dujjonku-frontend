96d01c2f298baa9219382c52b55df8ecd18bba42
chc216 <mine3386@naver.com>
2026-06-05 18:38:42 +0900

    refactor: 랜딩 페이지 추가에 따른 각 버튼별 네비게이션 경로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { Routes, Route } from 'react-router-dom'
-import Report from '@/pages/user/Report'
-import DashBoard from '@/pages/user/DashBoard'
-import Quiz from '@/pages/user/Quiz'
-import Main from '@/pages/user/Main'
+import { Routes, Route } from "react-router-dom";
+import Report from "@/pages/user/Report";
+import DashBoard from "@/pages/user/DashBoard";
+import Quiz from "@/pages/user/Quiz";
+import Main from "@/pages/user/Main";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/dashboard" element={<DashBoard />} />
-      <Route path="/report/:id" element={<Report />} />
+      <Route path="/report" element={<Report />} />
       <Route path="/quiz" element={<Quiz />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
