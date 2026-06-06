7f2234f54b4dd423efd1d1b5189bdd129795fa99
chc216 <mine3386@naver.com>
2026-06-06 16:19:41 +0900

    refactor: chart 컴포넌트 prop을 추가하여 유연한 디자인을 반영하도록 수정

--- src/components/FrequencyChart.jsx
+++ src/components/FrequencyChart.jsx
@@ -1,23 +1,34 @@
 import ReactApexChart from "react-apexcharts";
 
-function FrequencyChart({ frequency }) {
+function FrequencyChart({
+  frequency,
+  type = "area",
+  color = "#69cf24",
+  ygrid_show = true,
+  show_axis = true,
+}) {
   const entries = Object.entries(frequency).reverse();
   const list = [{ name: "빈도수", data: entries.map(([, count]) => count) }];
 
   const options = {
     chart: {
-      type: "area",
+      type: type,
       toolbar: { show: false },
       zoom: { enabled: false },
       fontFamily: "Plus Jakarta Sans",
     },
     dataLabels: { enabled: false },
     stroke: { curve: "smooth", width: 3 },
-    colors: ["#69cf24"],
+    colors: [color],
     grid: {
       borderColor: "#f1f1f1",
       strokeDashArray: 4,
-      xaxis: { lines: { show: false } },
+      xaxis: {
+        lines: { show: false },
+      },
+      yaxis: {
+        lines: { show: ygrid_show },
+      },
     },
     markers: { size: 0 },
     xaxis: {
@@ -27,10 +38,16 @@ function FrequencyChart({ frequency }) {
       }),
       axisBorder: { show: false },
       axisTicks: { show: false },
-      labels: { style: { colors: "#9e9e9e", fontSize: "12px" } },
+      labels: {
+        style: { colors: "#9e9e9e", fontSize: "12px" },
+        show: show_axis,
+      },
     },
     yaxis: {
-      labels: { style: { colors: "#9e9e9e", fontSize: "12px" } },
+      labels: {
+        style: { colors: "#9e9e9e", fontSize: "12px" },
+        show: show_axis,
+      },
     },
     tooltip: { theme: "light" },
   };
@@ -39,7 +56,7 @@ function FrequencyChart({ frequency }) {
     <ReactApexChart
       options={options}
       series={list}
-      type="area"
+      type={type}
       height="100%"
       width="100%"
     />
