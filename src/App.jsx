88efe84ccea93ad040691e5ee414683886fd3324
chc216 <mine3386@naver.com>
2026-06-10 22:54:02 +0900

    refactor: 단어 상세페이지 백엔드 연결 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,18 @@ import Report from "@/pages/user/Report";
 import DashBoard from "@/pages/user/DashBoard";
 import Quiz from "@/pages/user/Quiz";
 import Main from "@/pages/user/Main";
-import Admin from '@/pages/user/Admin/Admin'
-import AdminLogin from "@/pages/user/Admin/AdminLogin"
+import Admin from "@/pages/user/Admin/Admin";
+import AdminLogin from "@/pages/user/Admin/AdminLogin";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
       <Route path="/dashboard" element={<DashBoard />} />
-      <Route path="/report" element={<Report />} />
+      <Route path="/report/:id" element={<Report />} />
       <Route path="/quiz" element={<Quiz />} />
       <Route path="/admin" element={<Admin />} />
-      <Route path="/admin/login" element={<AdminLogin />}/>
+      <Route path="/admin/login" element={<AdminLogin />} />
     </Routes>
   );
 }
